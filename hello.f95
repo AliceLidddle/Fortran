32b83ecda82c635c68dcf264ec76322dd7fc6d81
Alice <nameno@byte-count.de>
2023-05-27 15:30:43 +0200

    Write Test

--- hello.f95
+++ hello.f95
@@ -9,12 +9,26 @@ program helloworld
     integer, dimension(10,10) :: array3
     integer :: array4(0:9)
     integer :: array5(-5:5)
+    integer :: i
 
     !write(*,*) "Hello World!"
     !print *, 'Hello World2!'
 
     print *, 'Please enter your age: '
     read(*,*) age
-
     print *, 'Your age is: ', age
+
+    print *, 'Hello World'
+
+    call hello(1.0)
+
+    do i=1,10
+        write (*, '(A)') "Hello"
+        write (*, '(A)') i
+    end do
+    
 end program helloworld
+
+subroutine hello(argument1)
+    print*, 'In Subroutine'
+end subroutine hello
