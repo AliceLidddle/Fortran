0cc8c398e111eb1362cf5dc57cb725d8d9c9b82b
Alice <alice@byte-count.de>
2023-05-27 15:40:57 +0200

    programmaufbau

--- hello.f95
+++ hello.f95
@@ -2,19 +2,25 @@ program helloworld
 
     !https://fortran-lang.org/en/learn/quickstart/variables/
     implicit none
-    !integer :: a, j, g
-    integer :: age
-    integer, dimension(10) :: array1
-    integer :: array2(10)
-    integer, dimension(10,10) :: array3
-    integer :: array4(0:9)
-    integer :: array5(-5:5)
+    !integer :: age
+    !integer, dimension(10) :: array1
+    !integer :: array2(10)
+    !integer, dimension(10,10) :: array3
+    !integer :: array4(0:9)
+    !integer :: array5(-5:5)
+    integer                 :: a, b
+    integer                 :: i(5) !List
+    integer                 :: j
+    integer                 :: st
+    real                    :: r
+    complex                 :: z
+    character (len = 10)    :: str
 
-    !write(*,*) "Hello World!"
-    !print *, 'Hello World2!'
+    write (*,*) 'Entr 5 Numbers'
+    read (*,*) i, st
+    write(*,*) 'IO-Status:', st
 
-    print *, 'Please enter your age: '
-    read(*,*) age
 
-    print *, 'Your age is: ', age
+    !Listengesteuerte Einabe per Tastatur
 end program helloworld
+
